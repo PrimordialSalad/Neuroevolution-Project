f2f42163750b990abf976bcac9264f85633f9192
John <johnyang@protonmail.ch>
2015-10-10 19:14:55 -0700

    Added more GA logic

--- Neuroevolution/src/MicrobialGeneticAlgorithm/MicrobialGA.java
+++ Neuroevolution/src/MicrobialGeneticAlgorithm/MicrobialGA.java
@@ -10,14 +10,15 @@ public class MicrobialGA
 	private Vector<NeuralNetwork> population = new Vector<NeuralNetwork>();
 	private int activeNetworkIndex;
 	private int competitorNetworkIndex;
-	private boolean sessionTermination;
-	private int mutationRate;
-	private int crossOverRate;
+	private final int MUTATION_RATE = 1;
+	private final int CROSSOVER_RATE = 50;
 	private double survalTimeFitnessWeight;
 	private double piecesSurvivedFitnessWeight;
 	private double scoreFitnessWeight;
-	private int trialCount;
-	
+	private double[] trialFitnessScore = {0, 0, 0};
+	private int sessionCount;
+	private int stageCount;
+	private final int MAX_SESSION_COUNT = 3;
 
 	public MicrobialGA(int setPopulationCount, int setInputCount, int setOutputCount, int setHiddenLayerCount, int setNeuronsPerHiddenLayer)
 	{
@@ -25,9 +26,6 @@ public class MicrobialGA
 		{
 			population.add(new NeuralNetwork(setInputCount, setOutputCount, setHiddenLayerCount, setNeuronsPerHiddenLayer));
 		}
-		crossOverRate = 50;
-		mutationRate = 1;
-		sessionTermination = false;
 		selectGenes();
 	}
 	
@@ -38,8 +36,55 @@ public class MicrobialGA
 	
 	public void teminateSession(double score, double piecesSurvied, double time)
 	{
-		population.get(activeNetworkIndex).setFitness(evaluateFitness(score, piecesSurvied, time));
-		trialCount++;
+		double sessionFitnessScore = evaluateFitness(score, piecesSurvied, time);
+		System.out.println("Session number: " + sessionCount + " teminated, neural network index: " 
+							+ activeNetworkIndex + " Session score: " + sessionFitnessScore);
+		trialFitnessScore[sessionCount] = sessionFitnessScore;
+		sessionCount++;
+		if(sessionCount > MAX_SESSION_COUNT)
+		{
+			teminateTrial();
+		}
+	}
+	
+	private void teminateTrial()
+	{
+		System.out.print("Trial termination on population index" + activeNetworkIndex + " ");
+		double averageFitness = 0;
+		for(int i = 0; i < trialFitnessScore.length; i++)
+		{
+			averageFitness += trialFitnessScore[i];
+		}
+		averageFitness /= MAX_SESSION_COUNT;
+		population.get(activeNetworkIndex).setFitness(averageFitness);
+		System.out.println("fitness score: " + averageFitness);
+		sessionCount = 0;
+		stageCount++;
+		if(stageCount > 1)
+		{
+			if(population.get(activeNetworkIndex).getFitness() > population.get(competitorNetworkIndex).getFitness())
+			{
+				infect(population.get(activeNetworkIndex), population.get(competitorNetworkIndex));
+				mutate(population.get(competitorNetworkIndex));
+			}
+			else if(population.get(activeNetworkIndex).getFitness() < population.get(competitorNetworkIndex).getFitness())
+			{
+				infect(population.get(competitorNetworkIndex), population.get(activeNetworkIndex));
+				mutate(population.get(activeNetworkIndex));
+			}
+			else
+			{
+				mutate(population.get(competitorNetworkIndex));
+				mutate(population.get(activeNetworkIndex));
+			}
+			selectGenes();
+		}
+		else
+		{
+			int indexPlaceHolder = activeNetworkIndex;
+			activeNetworkIndex = competitorNetworkIndex;
+			competitorNetworkIndex = indexPlaceHolder;
+		}
 	}
 	
 	public double evaluateFitness(double score, double piecesSurvived, double time)
@@ -49,6 +94,12 @@ public class MicrobialGA
 	
 	public void selectGenes()
 	{
+		for(int i = 0; i < trialFitnessScore.length; i++)
+		{
+			trialFitnessScore[i] = 0;
+		}
+		stageCount = 0;
+		sessionCount = 0;
 		activeNetworkIndex = random.nextInt(population.size());
 		competitorNetworkIndex = random.nextInt(population.size());
 		while(competitorNetworkIndex == activeNetworkIndex)
@@ -63,7 +114,7 @@ public class MicrobialGA
 		Vector<Double> loserGene = loser.getWeights();
 		for(int i = 0; i < winnerGene.size(); i++)
 		{
-			if(random.nextInt(101) < crossOverRate)
+			if(random.nextInt(100) < CROSSOVER_RATE)
 			{
 				double newChromosome = winnerGene.get(i);
 				System.out.println("Crossover at gene index: " + i + ", now set to: " + newChromosome);
@@ -80,7 +131,7 @@ public class MicrobialGA
 		Vector<Double> nnGene = nn.getWeights();
 		for(int i = 0; i < nnGene.size(); i++)
 		{
-			if(random.nextInt(101) < (mutationRate / (double)nn.getWeights().size()))
+			if(random.nextInt(100) < MUTATION_RATE)
 			{
 				double newChromosome = random.nextDouble() * (1.0-(-1.0)) - 1.0;
 				System.out.println("Mutation at gene index: " + i + ", now set to: " + newChromosome);
@@ -91,9 +142,5 @@ public class MicrobialGA
 		System.out.println("Weights after mutation:");
 		nn.printStatusToConsole();
 	}
-	
-	
-	
-	
 	
 }
